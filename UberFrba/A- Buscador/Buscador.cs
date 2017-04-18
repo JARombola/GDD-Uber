32bdf6c7623784f49bb0a2e659b9075ffd82a7eb
Julian R <juli_fula@hotmail.com>
2017-04-18 20:21:04 -0300

    Arrancando proyecto...

---
+++ UberFrba/A- Buscador/Buscador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UberFrba.A__Buscador {
+    class Buscador {
+        private static Buscador instancia{ get; set; }
+        private SqlConnection conexion;
+        private string DATOS_USUARIO = "user id=gd; password=gd2017";
+        private string BASE = "database=Animales; ";
+ 
+
+        private Buscador () {
+  //        string direccion = obtenerDireccionBD();
+            String config = "server=LocalHost\\SQLSERVER2012; "+ BASE+DATOS_USUARIO;
+            conexion = new SqlConnection(config);
+            try { 
+                conexion.Open();
+               MessageBox.Show("CONEXION OK!");
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Fallo la conexion");
+            }
+        }
+
+        private String obtenerDireccionBD () {
+            StreamReader file = new StreamReader(@"..\..\configBD.txt");
+            String linea = file.ReadLine();
+            int inicioURL=linea.IndexOf("=")+1;
+            linea = linea.Substring(inicioURL, linea.Length-inicioURL);
+            linea = linea.Trim();
+        return linea;
+        }
+
+        public static Buscador getInstancia(){
+            if (instancia == null) instancia = new Buscador();
+            return instancia;
+        }
+
+        public void ejecutarQuery (string query, DataGridView dataGrid) {
+            SqlCommand command= new SqlCommand(query, conexion);
+            ConfiguradorDG config = new ConfiguradorDG();
+            config.completarDataGrid(dataGrid, command);
+           // return data;
+        }
+    }
+}
---
+++ UberFrba/A- Buscador/ConfiguradorDG.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UberFrba.A__Buscador {
+    class ConfiguradorDG {
+        public ConfiguradorDG (){}
+
+        public void completarDataGrid (DataGridView dataGrid, SqlCommand query) {
+            MessageBox.Show("CONSULTA: \n"+query.CommandText);
+            DataTable tabla = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(query);
+            adapter.Fill(tabla);
+            dataGrid.DataSource = tabla;
+        }
+
+
+    }
+}
